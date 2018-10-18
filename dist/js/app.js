2dc649f0397014451716c9976b0f27f039451e23
Ben Patterson <bmizepatterson@gmail.com>
2018-10-18 10:57:53 -0400

    Add a contact

--- dist/js/app.js
+++ dist/js/app.js
@@ -6,6 +6,8 @@ let app = new Vue({
 
     data: {
 
+        nextId: 3,
+
         contacts: [
             {
                 id: 1,
@@ -21,11 +23,47 @@ let app = new Vue({
                 emailaddr: 'janed@example.com',
                 phonenum: '859-123-4567'
             }
-        ]
+        ],
+
+        buttonText: 'Add',
+
+        showForm: true,
+
+        formFirst: '',
+        formLast: '',
+        formEmail: '',
+        formPhone: ''
 
     },
 
     methods: {
+
+        addOrUpdateContact: function() {
+
+            // validate input
+
+            let newContact = {
+                id: this.nextId,
+                firstname: this.formFirst,
+                lastname: this.formLast,
+                emailaddr: this.formEmail,
+                phonenum: this.formPhone
+            };
+
+            this.contacts.push(newContact);
+
+            this.formFirst = this.formLast = this.formEmail = this.formPhone = '';
+
+            this.nextId++;
+        },
+
+        sortContacts: function(field) {
+            console.log("Sorting contacts by [" + field + "]");
+        },
+
+        toggleForm: function() {
+            this.showForm = !this.showForm;
+        }
 
     }
 
